f6558859fc868be544bf06adc02e125d54d7906c
Anthony Headley <agheadley@gmail.com>
2025-01-07 15:48:33 +0000

    building results view

---
+++ src/routes/overview/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import * as icon from '$lib/icon';
+    import {cohorts,config,user,alert} from '$lib/state.svelte';
+    
+   
+    
+    $effect(() => {  
+        (async () => {
+            if(!user.isTeacher || config.isReady===false) goto(`/`);
+        })()
+    });
+    
+    
+    
+        
+    </script>
+    
+    <svelte:head>
+        <title>Overview</title>
+        <meta name="description" content="ailanthus" />
+    </svelte:head>
+    
+    <p>ROverview</p>
+
+
+    <style>
+    
+    
+    
+    </style>
+    
---
+++ src/routes/reports/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import * as icon from '$lib/icon';
+    import {cohorts,config,user,alert} from '$lib/state.svelte';
+    
+   
+    
+    $effect(() => {  
+        (async () => {
+            if(!user.isTeacher || config.isReady===false) goto(`/`);
+        })()
+    });
+    
+    
+    
+        
+    </script>
+    
+    <svelte:head>
+        <title>Reports</title>
+        <meta name="description" content="ailanthus" />
+    </svelte:head>
+    
+    <p>Reports</p>
+
+
+    <style>
+    
+    
+    
+    </style>
+    
